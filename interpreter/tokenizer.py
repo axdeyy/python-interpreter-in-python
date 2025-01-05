b226b42816338f588efd2f9f5d8a528533e8aa56
Aalaap Dey <aalaapdey@gmail.com>
2025-01-05 17:49:09 +0000

    feat: define global token specifications list

--- interpreter/tokenizer.py
+++ interpreter/tokenizer.py
@@ -22,5 +22,15 @@ class TokenSpecification:
     category: TokenCategory
     regex_pattern: str
 
+TOKEN_SPECIFICATIONS: List[TokenSpecification] = [
+    TokenSpecification(TokenCategory.IDENTIFIER, r'[A-Za-z_]\w*'),
+    TokenSpecification(TokenCategory.KEYWORD, r'\bprint\b'),  # TODO: add more keywords
+    TokenSpecification(TokenCategory.NUMBER, r'\d+'),    
+    TokenSpecification(TokenCategory.OPERATOR, r'[+=-]'),
+    TokenSpecification(TokenCategory.PAREN, r'[()]'),
+    TokenSpecification(TokenCategory.NEWLINE, r'\n'),
+    TokenSpecification(TokenCategory.MISMATCH, r'.'),
+]
+
 def tokenize(source_code: str) -> List[Token]:
     pass
