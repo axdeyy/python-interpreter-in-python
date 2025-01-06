251e3f75d6fc99e830aa58b37d112677a081a7cc
Aalaap Dey <aalaapdey@gmail.com>
2025-01-06 10:38:04 +0000

    test: add temporary tokenizer test to run from main.py

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
 # main.py
 
+from interpreter.tokenizer import *
+
+def test_tokenizer():
+    source_code = """x = 10
+    y = 20
+    z = x + y
+    print(z)"""
+
+    tokens = tokenize(source_code)
+    
+    for token in tokens:
+        print(token)
+
+if __name__ == "__main__":
+    test_tokenizer()
