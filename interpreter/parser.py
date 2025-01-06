b72cc3317cccfba94b2d0257b4de8a7d3fa0ad0c
Aalaap Dey <aalaapdey@gmail.com>
2025-01-06 19:03:20 +0000

    feat: create Parser class and define __init__ function

--- interpreter/parser.py
+++ interpreter/parser.py
@@ -1,1 +1,10 @@
 # intepreter/parser.py
+
+import token
+from lexer import Token
+
+class Parser:
+    def __init__(self, tokens: list[Token]):
+        self.tokens = tokens
+        self.current_token_idx = 0
+    
