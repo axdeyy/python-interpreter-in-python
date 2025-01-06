331793fffeacc12b3ee38fcea5e02f3ea92b4039
Aalaap Dey <aalaapdey@gmail.com>
2025-01-06 18:26:03 +0000

    feat: add Assignment AST node

--- interpreter/ast.py
+++ interpreter/ast.py
@@ -16,8 +16,15 @@ literal            ::= NUMBER
 '''
 
 from abc import ABC
+from ast import expr
 from dataclasses import dataclass
+from lexer import TokenCategory
 
 @dataclass
 class Expression(ABC):
     pass
+
+@dataclass
+class Assignment:
+    identifier: TokenCategory.IDENTIFIER
+    expression: Expression
