d813af651a89e00b144320148405c553b0b40544
Aalaap Dey <aalaapdey@gmail.com>
2025-01-07 19:33:20 +0000

    test: add operators parameter set

--- tests/test_lexer.py
+++ tests/test_lexer.py
@@ -6,6 +6,11 @@ from interpreter.lexer import tokenize, Token, TokenCategory
 @pytest.mark.parametrize("source_code, expected_tokens", [
     ("print", [Token(category=TokenCategory.KEYWORD, lexeme="print")]),
     ("variable_name", [Token(category=TokenCategory.IDENTIFIER, lexeme="variable_name")]),
+    ("+ - =", [
+        Token(category=TokenCategory.OPERATOR, lexeme="+"),
+        Token(category=TokenCategory.OPERATOR, lexeme="-"),
+        Token(category=TokenCategory.OPERATOR, lexeme="=")
+    ]),
 ])
 def test_tokenize_cases(source_code, expected_tokens):
     assert tokenize(source_code) == expected_tokens
