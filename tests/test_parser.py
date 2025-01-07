d90587c58abee31d75c421a607fe7000e9c3b434
Aalaap Dey <aalaapdey@gmail.com>
2025-01-07 20:11:57 +0000

    test: add assignment parsing test function

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,1 +1,21 @@
 # test/test_parser.py
+
+from re import A
+import pytest
+from interpreter.ast import Assignment, Literal
+from interpreter.lexer import Token, TokenCategory
+from interpreter.parser import Parser
+
+def test_parse_assignment(
+    tokens: list[Token],
+    expected_identifier: str,
+    expected_value: int
+) -> None:
+    parser = Parser(tokens)
+    program = parser.parse()
+    statement = program.statements[0]
+    
+    assert isinstance(statement, Assignment)
+    assert statement.identifier == expected_identifier
+    assert isinstance(statement.expression, Literal)
+    assert statement.expression.value == expected_value
