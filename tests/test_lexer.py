833b8469b8e93532f45e281696bbd1e2ae49b01e
Aalaap Dey <aalaapdey@gmail.com>
2025-01-07 19:33:04 +0000

    test: add identifier parameter set

--- tests/test_lexer.py
+++ tests/test_lexer.py
@@ -4,7 +4,8 @@ import pytest
 from interpreter.lexer import tokenize, Token, TokenCategory
 
 @pytest.mark.parametrize("source_code, expected_tokens", [
-    ("print", [Token(category=TokenCategory.KEYWORD, lexeme="print")])
+    ("print", [Token(category=TokenCategory.KEYWORD, lexeme="print")]),
+    ("variable_name", [Token(category=TokenCategory.IDENTIFIER, lexeme="variable_name")]),
 ])
 def test_tokenize_cases(source_code, expected_tokens):
     assert tokenize(source_code) == expected_tokens
