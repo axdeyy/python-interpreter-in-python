d5637fd6750efc3a37cdc8a8fb3e7f54319b7a6d
Aalaap Dey <aalaapdey@gmail.com>
2025-01-07 19:37:40 +0000

    test: add variable assignment to identifier parameter set

--- tests/test_lexer.py
+++ tests/test_lexer.py
@@ -23,11 +23,17 @@ from interpreter.lexer import tokenize, Token, TokenCategory
         Token(category=TokenCategory.IDENTIFIER, lexeme="x"),
         Token(category=TokenCategory.PAREN, lexeme=")"),
     ]),
-("x = 10\n", [
-        Token(category=TokenCategory.IDENTIFIER, lexeme="x"),
-        Token(category=TokenCategory.OPERATOR, lexeme="="),
-        Token(category=TokenCategory.NUMBER, lexeme=10),
-        Token(category=TokenCategory.NEWLINE, lexeme="\n")
+    ("x = 10\n", [
+            Token(category=TokenCategory.IDENTIFIER, lexeme="x"),
+            Token(category=TokenCategory.OPERATOR, lexeme="="),
+            Token(category=TokenCategory.NUMBER, lexeme=10),
+            Token(category=TokenCategory.NEWLINE, lexeme="\n")
+    ]),
+    ("x = y\n", [
+            Token(category=TokenCategory.IDENTIFIER, lexeme="x"),
+            Token(category=TokenCategory.OPERATOR, lexeme="="),
+            Token(category=TokenCategory.IDENTIFIER, lexeme="y"),
+            Token(category=TokenCategory.NEWLINE, lexeme="\n")
     ]),
 ])
 def test_tokenize_cases(source_code, expected_tokens):
