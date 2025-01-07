a83717bbf5de83203afb187e1ce51c5216e36615
Aalaap Dey <aalaapdey@gmail.com>
2025-01-07 19:15:35 +0000

    test: add tokenize function test case

--- tests/test_lexer.py
+++ tests/test_lexer.py
@@ -1,1 +1,7 @@
 # test/test_lexer.py
+
+import pytest
+from interpreter.lexer import tokenize, Token, TokenCategory
+
+def test_tokenize_cases(source_code, expected_tokens):
+    assert tokenize(source_code) == expected_tokens
