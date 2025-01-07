aa32baaf0ab253be18291de9ef765762d77d1cde
Aalaap Dey <aalaapdey@gmail.com>
2025-01-07 19:23:48 +0000

    test: add pytest parametrization decorator and print keyword parameter set

--- tests/test_lexer.py
+++ tests/test_lexer.py
@@ -3,5 +3,8 @@
 import pytest
 from interpreter.lexer import tokenize, Token, TokenCategory
 
+@pytest.mark.parametrize("source_code, expected_tokens", [
+    ("print", [Token(category=TokenCategory.KEYWORD, lexeme="print")])
+])
 def test_tokenize_cases(source_code, expected_tokens):
     assert tokenize(source_code) == expected_tokens
