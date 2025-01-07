5af36a9faa801f62830d74687848c6694f4fe888
Aalaap Dey <aalaapdey@gmail.com>
2025-01-07 19:15:05 +0000

    test: add temporary testing of parser in main.py

--- main.py
+++ main.py
@@ -1,17 +1,33 @@
 # main.py
 
 from interpreter.lexer import *
+from interpreter.parser import *
 
 def test_tokenizer():
-    source_code = """x = 10
-    y = 20
-    z = x + y
-    print(z)"""
+    source_code = """
+    z = 0
+
+    f(x)
+    
+    x = 2 + 3 + 2
+    print(x)
+
+    print(2)
+    
+    """
 
     tokens = tokenize(source_code)
     
-    for token in tokens:
-        print(token)
+    return tokens
+
+def test_parser(tokens: list[Token]):
+    p = Parser(tokens)
+    return p.parse()
 
 if __name__ == "__main__":
-    test_tokenizer()
+    tokens = test_tokenizer()
+    print(test_parser(tokens))
+
+
+# Program(statements=[Assignment(identifier='x', expression=BinaryExpression(left=Literal(value=2), operator=Token(category=<TokenCategory.OPERATOR: 4>, lexeme='+'), right=BinaryExpression(left=Literal(value=3), operator=Token(category=<TokenCategory.OPERATOR: 4>, lexeme='+'), right=Literal(value=2)))), FunctionCall(name=Token(category=<TokenCategory.KEYWORD: 2>, lexeme='print'), arguments=[Variable(name=Token(category=<TokenCategory.IDENTIFIER: 1>, lexeme='x'))]), FunctionCall(name=Token(category=<TokenCategory.KEYWORD: 
+# 2>, lexeme='print'), arguments=[Literal(value=2)])])
