4d40e08b58ba0f453b4e58f316a089f270e46f4e
Aalaap Dey <aalaapdey@gmail.com>
2025-01-12 15:29:16 +0000

    refactor: remove token specification dataclass

--- interpreter/lexer/token.py
+++ interpreter/lexer/token.py
@@ -5,8 +5,3 @@ from .token_categories import TokenCategory
 class Token:
     category: TokenCategory
     lexeme: int | str
-
-@dataclass
-class TokenSpecification:
-    category: TokenCategory
-    regex_pattern: str
