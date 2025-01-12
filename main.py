25a600a7a9510a9feceda4f7c30bc646b2b207b1
Aalaap Dey <aalaapdey@gmail.com>
2025-01-12 19:39:07 +0000

    refactor: remove old comments

--- main.py
+++ main.py
@@ -30,7 +30,3 @@ if __name__ == "__main__":
     tokens = test_tokenizer()
     print(tokens)
     print(test_parser(tokens))
-
-
-# Program(statements=[Assignment(identifier='x', expression=BinaryExpression(left=Literal(value=2), operator=Token(category=<TokenCategory.OPERATOR: 4>, lexeme='+'), right=BinaryExpression(left=Literal(value=3), operator=Token(category=<TokenCategory.OPERATOR: 4>, lexeme='+'), right=Literal(value=2)))), FunctionCall(name=Token(category=<TokenCategory.KEYWORD: 2>, lexeme='print'), arguments=[Variable(name=Token(category=<TokenCategory.IDENTIFIER: 1>, lexeme='x'))]), FunctionCall(name=Token(category=<TokenCategory.KEYWORD: 
-# 2>, lexeme='print'), arguments=[Literal(value=2)])])
