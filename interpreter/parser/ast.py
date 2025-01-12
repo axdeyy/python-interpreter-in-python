aceb240307ffa8d0f96e8864246a34518af15105
Aalaap Dey <aalaapdey@gmail.com>
2025-01-12 15:52:43 +0000

    refactor: move ast.py file from interpreter package to parser subpackage

--- interpreter/parser/ast.py
+++ interpreter/parser/ast.py
@@ -18,7 +18,7 @@ literal            ::= TokenCategory.NUMBER
 
 from abc import ABC
 from dataclasses import dataclass
-from .lexer.tokenizer import Token
+from ..lexer.tokenizer import Token
 
 @dataclass(frozen=True)
 class Statement(ABC):
