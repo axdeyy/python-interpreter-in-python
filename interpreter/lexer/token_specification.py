f46d582267e05fd7d5ce1db0eda697e7cce7ee60
Aalaap Dey <aalaapdey@gmail.com>
2025-01-12 15:33:59 +0000

    feat: extract token specification dataclass and TOKEN_SPECIFICATIONS list to separate file and add multiple keyword handling

---
+++ interpreter/lexer/token_specification.py
@@ -0,0 +1,30 @@
+import re
+from dataclasses import dataclass
+from .token_categories import TokenCategory
+
+@dataclass
+class TokenSpecification:
+    category: TokenCategory
+    regex_pattern: str
+
+KEYWORDS = {
+    "print",
+    "sum"
+}
+
+# Build KEYWORD regex pattern
+keyword_pattern = r'\b(?:' + '|'.join(map(re.escape, KEYWORDS)) + r')\b'
+
+# Token specifications
+TOKEN_SPECIFICATIONS = [
+    TokenSpecification(TokenCategory.KEYWORD, keyword_pattern),  # Dynamic keyword pattern
+    TokenSpecification(TokenCategory.IDENTIFIER, r'[A-Za-z_]\w*'),
+    TokenSpecification(TokenCategory.NUMBER, r'\d+'),
+    TokenSpecification(TokenCategory.OPERATOR, r'[+=-]'),
+    TokenSpecification(TokenCategory.OPEN_PAREN, r'\('),
+    TokenSpecification(TokenCategory.CLOSE_PAREN, r'\)'),
+    TokenSpecification(TokenCategory.NEWLINE, r'\n'),
+    TokenSpecification(TokenCategory.SKIP, r'\s+'),
+    TokenSpecification(TokenCategory.COMMA, r','),
+    TokenSpecification(TokenCategory.MISMATCH, r'.'),
+]
